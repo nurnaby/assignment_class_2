0d5752e70d38c1b00fd70e46606103c391118766
nurnaby <97038168+nurnaby@users.noreply.github.com>
2022-03-24 12:42:05 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$discrption="Let's introduct yourself";
+$name = "Nur Naby sohag";//data type string
+$age  ="34.5";// data type float
+$addrs="bogra";//data type strintg
+$number=8801746493217;//data type integer
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>assignment_class_2</title>
+</head>
+
+<body>
+    <h3>Introduce yourself</h3>
+    <p>I am <?php echo $name ?> .I am <?php echo $age?>years old.My home district name is <?php echo $addrs?>.My phone
+        number<?php echo $number ?> </p>
+</body>
+
+</html>
